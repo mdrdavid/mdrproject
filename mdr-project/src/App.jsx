477452b015c5b632b75da70d2da19b84cdd79a39
david <davidmatovu88@gmail.com>
2022-11-17 05:45:29 +0300

    Add Filter to rooms table

--- mdr-project/src/App.jsx
+++ mdr-project/src/App.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
+import { Navbar, Starts } from './components'
 
-const App = () => {
-  return (
-    <div>Hello Vite</div>
+import styles from './styles'
+const App = () => 
+  (
+    <div className='bg-primary w-full overflow-hidden'>
+ 
+<div className={`${styles.paddingX} ${styles.flexCenter }`}>
+  <div className={`${styles.boxWidth}`}>
+    <Navbar/>
+  </div>
+</div>
+
+<div className={`bg-primary ${styles.flexStart}`}>
+  <div className={`${styles.boxWidth }`}>Hero component</div>
+</div>
+
+<div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+  <div className={`${styles.boxWidth }`}>
+   <Starts/>
+    Business
+    Billing
+    CardDeal
+    Testimonials
+    clients
+    CTA
+    Footer 
+    </div>
+</div>
+
+    </div>
   )
-}
-
 export default App
