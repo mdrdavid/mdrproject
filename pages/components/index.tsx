bad8bf09dc81157560d2fc6d4537d2e7c0be23a1
david <davidmatovu88@gmail.com>
2023-05-02 10:54:42 +0300

    remove testimonials

--- pages/components/index.tsx
+++ pages/components/index.tsx
@@ -6,6 +6,5 @@ export { default as Hero } from "./ui/Hero/Hero";
 export { default as Navbar } from "./ui/Navbar/Navbar";
 export { default as Starts } from "./ui/Starts/Starts";
 export { default as ContactUs } from "./ui/Contact/ContactUs";
-export { default as Testimonials } from "./ui/Testimonials/Testmonials";
 export { default as Footer } from "./ui/Footer/Footer";
 export { default as Services } from "./ui/Services/Services";
